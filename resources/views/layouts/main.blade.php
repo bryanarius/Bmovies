bd48205ad7f0d1c7fe5592e8394e57107ae83014
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-14 00:34:06 -0400

    Initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @vite('resources/css/app.css')
+        <title>Bmovies</title>
+    
+    </head>
+    <body>
+        <nav>Nav goes here</nav>
+        @yield('content')
+    </body>
+</html>
