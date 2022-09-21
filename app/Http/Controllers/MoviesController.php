dd1636e5386c0b29846c01d8e782d44d81d4315a
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-21 06:34:26 -0400

    Refactoring View Models
    - Added Movies View Model for more efficient code on index page
    
    - Added Movie View Model for more efficient code on show page
    
    - CSS changes
    
    -Bug Fixes

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use App\Models\Movie;
+use Illuminate\Http\Request;
+use App\ViewModels\MovieViewModel;
+use App\ViewModels\MoviesViewModel;
+use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
@@ -16,28 +18,33 @@ class MoviesController extends Controller
         $nowPlayingMovies = http::get("https://api.themoviedb.org/3/movie/now_playing?api_key=52ffb2d1c618379d189e12ac312404be")
             ->json()['results'];
 
-        $genresArray = http::get("https://api.themoviedb.org/3/genre/movie/list?api_key=52ffb2d1c618379d189e12ac312404be")
+        $genres = http::get("https://api.themoviedb.org/3/genre/movie/list?api_key=52ffb2d1c618379d189e12ac312404be")
             ->json()['genres'];
 
-        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
-            return [$genre['id'] => $genre['name']];
-        });
+        
 
+    //     return view('index', [
+    //         'popularMovies' => $popularMovies,
+    //         'nowPlayingMovies' => $nowPlayingMovies,
+    //         'genres' => $genres,
+    //     ]);
+    // }
 
-        return view('index', [
-            'popularMovies' => $popularMovies,
-            'nowPlayingMovies' => $nowPlayingMovies,
-            'genres' => $genres,
-        ]);
+    $viewModel = new MoviesViewModel(
+        $popularMovies,
+        $nowPlayingMovies,
+        $genres
+    );
+
+     return view('index', $viewModel);
     }
 
     public function show($movie) {
         $movie = http::get("https://api.themoviedb.org/3/movie/".$movie."?api_key=52ffb2d1c618379d189e12ac312404be&append_to_response=videos,images,credits")
         ->json();
 
+        $viewModel = new MovieViewModel($movie);
 
-        return view('show', [
-            'movie' => $movie
-        ]);
+        return view('show', $viewModel);
     }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,18 +4,18 @@
 
     <div class="movie-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-            <img src="{{'https://image.tmdb.org/t/p/w500'.$movie['poster_path']}}" alt="movie" class="w-64 lg:w-96">
+            <img src="{{$movie['poster_path']}}" alt="movie" class="w-64 lg:w-96">
            <div class="md:ml-24">
                 <h2 class="text-4xl font-semibold">{{$movie['title']}}</h2> 
                 <div class="div flex flex-wrap items-center text-gray-400 text-sm">
                     <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
-                    <span class="ml-1">{{ $movie['vote_average'] * 10 . "%" }}</span>
+                    <span class="ml-1">{{$movie['vote_average']}}</span>
                     <span class="mx-2">|</span>
-                    <span>{{\Carbon\Carbon::parse($movie['release_date'])->format('M d, Y')}}</span>
+                    <span>{{$movie['release_date']}}</span>
                     <span class="mx-2">|</span>
-                    <span> @foreach ($movie['genres'] as $genre)
-                        {{ $genre['name']}} @if (!$loop->last), @endif
-                    @endforeach</span>
+                    <span> 
+                        {{$movie['genres']}}
+                    </span>
                 </div>
 
                 <p class="text-gray-300 mt-8">
@@ -25,13 +25,11 @@
                 <div class="mt-12">
                     <h4 class="text-white font-semibold">Featured Cast</h4>
                     <div class="flex mt-4">
-                        @foreach ($movie['credits']['crew'] as $crew)
-                        @if ($loop->index < 2)
-                        <div class="mr-8">
-                            <div>{{$crew['name']}}</div>
-                            <div class="text-sm text-gray-400">{{$crew['job']}}</div>
-                        </div>
-                        @endif
+                        @foreach ($movie['crew'] as $crew)
+                            <div class="mr-8">
+                                <div>{{$crew['name']}}</div>
+                                <div class="text-sm text-gray-400">{{$crew['job']}}</div>
+                            </div>
 
                         @endforeach
 
@@ -50,7 +48,7 @@
                                 <span class="ml-2">Play Trailer</span>
                             </button>
                         </div>
-                    @endif
+                    
 
                     <div
                         style="background-color: rgba(0, 0, 0, .5);"
@@ -68,12 +66,13 @@
                                 </div>
                                 <div class="modal-body px-8 py-8">
                                     <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
-                                        <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{$movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    @endif
                 </div>
 
            </div>
@@ -86,9 +85,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                 
 
-            @foreach ($movie['credits']['cast'] as $cast)
-
-            @if ($loop-> index < 5)
+            @foreach ($movie['cast'] as $cast)
                 <div class="mt-8">
                         <a href="#">
                             <img src="{{'https://image.tmdb.org/t/p/w300'.$cast['profile_path']}}" alt="Cast" class="hover:opacity-75 transition ease-in-out duration-150">
@@ -100,9 +97,9 @@
                             {{$cast['character']}}
                         </div>
                     </div>
-                </div> 
-            @endif
+                </div>
             @endforeach
+
         </div>
     </div>  <!-- End of Cast Area -->
 
@@ -111,8 +108,8 @@
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Images</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                @foreach ($movie['images']['backdrops'] as $image)
-                    @if ($loop-> index < 9)
+                @foreach ($movie['images'] as $image)
+                   
                         <div class="mt-8">
                             <a 
                                 @click.prevent="isOpen = true"
@@ -122,7 +119,6 @@
                                     <img src="{{'https://image.tmdb.org/t/p/w500'.$image['file_path']}}" alt="image" class="hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                         </div>
-                    @endif
                 @endforeach
             </div>
 
